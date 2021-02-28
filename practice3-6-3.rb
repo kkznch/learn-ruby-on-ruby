fafcb95b3d3d5ad138d3e9f32003f92f6a93c458
kkznch <kkznch710md@gmail.com>
2021-02-28 18:20:44 +0900

    Chapter3

---
+++ practice3-6-3.rb
@@ -0,0 +1,10 @@
+def preorder(tree)
+  if tree[0].start_with?('節')
+    preorder(tree[1])
+    preorder(tree[2])
+  end
+  p(tree[0])
+end
+
+node1 = ['節1', ['節2', ['葉A'], ['葉B']], ['節3', ['葉C'], ['葉D']]]
+preorder(node1)
