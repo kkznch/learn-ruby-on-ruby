d50adf85dc77657f3df5d297f93be2222d9cc822
kkznch <kkznch710md@gmail.com>
2021-03-07 16:46:09 +0900

    Chapter8

--- interp.rb
+++ interp.rb
@@ -24,6 +24,8 @@ def evaluate(tree, genv, lenv)
     evaluate(tree[1], genv, lenv) > evaluate(tree[2], genv, lenv)
   when '>='
     evaluate(tree[1], genv, lenv) >= evaluate(tree[2], genv, lenv)
+  when 'func_def'
+    genv[tree[1]] = ['user_defined', tree[2], tree[3]]
   when 'func_call'
     args = []
     i = 0
@@ -35,6 +37,14 @@ def evaluate(tree, genv, lenv)
     if mhd[0] == 'builtin'
       minruby_call(mhd[1], args)
     else
+      new_lenv = {}
+      params = mhd[1]
+      i = 0
+      while params[i]
+        new_lenv[params[i]] = args[i]
+        i = i + 1
+      end
+      evaluate(mhd[2], genv, new_lenv)
     end
   when 'stmts'
     i = 1
